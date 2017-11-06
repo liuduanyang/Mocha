eba84ac20c674fcebbc21773e23b3ef5db4e7cd7
liuduanyang <1069831692@qq.com>
2017-11-06 19:18:51 +0800

    提交demo

---
+++ m_2_1.js
@@ -0,0 +1,10 @@
+//创建被测项目
+
+//初始化仓库  git: git init
+//上传仓库时忽视依赖文件 git：touch .gitignore 并在其文件内填入屏蔽的文件或文件夹
+//使用npm初始化项目 命令行：npm init    得到package.json文件
+
+//添加示例代码文件 calc.js
+
+//安装Mocha 命令行：npm install mocha -D    安装到mocha-demo 下的 node_modules文件夹下
+//安装chai  命令行：npm install chai -D
---
+++ mocha-demo/index.js
@@ -0,0 +1,9 @@
+//主程序
+
+var add=require("./calc").add;
+var x=0;
+var y=0;
+var z=add(x,y);
+
+console.log('x+y='+z);
+//12.30
