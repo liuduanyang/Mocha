01fa88a33590cd628481a6d92f9f98354c5c6684
liuduanyang <1069831692@qq.com>
2017-11-08 10:18:31 +0800

    异步超时测试及简单异步测试

---
+++ m_13_1.js
@@ -0,0 +1,12 @@
+//技术细节二
+//异步测试 浏览器前端测试 ES6测试
+
+//异步测试
+//异步超时设置 简单异步测试(以回调函数为例) Promise异步测试
+
+//异步超时设置 及 简单异步测试
+//以timeout.js timeout.test.js 为例
+
+//Promise异步测试
+//需先安装第三方依赖库 命令行：npm install node-fetch -D  用于发起ajax请求
+//以promise.test.js 为例
