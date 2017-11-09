3cd4e5e65f7e88d4594cfc2eaef1b63314687c2d
liuduanyang <1069831692@qq.com>
2017-11-09 16:38:15 +0800

    webdriver api操作网站及与mocha的整合

---
+++ m_17_1.js
@@ -0,0 +1,21 @@
+//使用webdriver api来操作网站
+
+
+//安装 selenium-webdriver第三方库
+//命令行：npm install selenium-webdriver -D
+
+//举个栗子：在test文件夹内创建脚本 redmine-login.test.js
+
+//根据录制回放脚本(相当于该网站的使用步骤) ： http://seide.wangding.in/hostedredmine/login.html
+
+//编写完脚本后 执行该脚本
+//命令行：node test/redmine-login.test.js
+
+
+
+
+//mocha与webdriver整合
+//因为webdriver中没有断言的api 所以需要和mocha来进行整合
+//在test文件夹内创建一个 redmine-mocha.test.js文件
+
+
